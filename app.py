ea991db32f7fc43a52ff02b30158c298b6fa2d15
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-05-04 06:24:09 +0000

    fatto es 1

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import geopandas as gpd
+import contextily as ctx
+import matplotlib.pyplot as plt
+from shapely.geometry import Point,Polygon
+
 app = Flask(__name__)
 
-df=pd.read_excel("https://github.com/PolisenoRiccardo/perilPopolo/blob/main/milano_housing_02_2_23.xlsx?raw=true")
+df=pd.read_csv("http://servizi.apss.tn.it/opendata/FARM001.csv")
+comuni=gpd.read_file("/workspace/geopandas-Flask-sjoint-merge/Com01012022_g.zip")
 
+df2=df[(df.LATITUDINE_P!="-") & (df.LONGITUDINE_P!="-") ]
+geometry = [Point(xy) for xy in zip(df2.LONGITUDINE_P, df2.LATITUDINE_P)]
+geodf = gpd.GeoDataFrame(df2, crs=4326, geometry=geometry)
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/es1')
+def es1():
+    trovato=geodf.to_html()
+    return render_template("risultati.html",risultato=trovato )
 
 if __name__ == '__main__':
     app.run(debug=True)
