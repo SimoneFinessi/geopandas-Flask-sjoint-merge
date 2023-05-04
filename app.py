f119617164b0925c552e4e621470f0096a96b7a2
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-05-04 06:50:38 +0000

    finire es 2

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 import pandas as pd
 import geopandas as gpd
 import contextily as ctx
 import matplotlib.pyplot as plt
 from shapely.geometry import Point,Polygon
+import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 app = Flask(__name__)
 
@@ -22,5 +25,19 @@ def es1():
     trovato=geodf.to_html()
     return render_template("risultati.html",risultato=trovato )
 
+@app.route('/es2', methods = ['POST'])
+def es2():
+    farm=request.form["farmacia"]
+    return render_template("es2.html",farmacia=farm)
+
+@app.route("/immagineEs2", methods = ['POST'])
+def immagineEs2():
+    farm=request.form["farmacia"]
+    fig,ax= plt.subplot()
+    geodf[geodf.FARMACIA.str.contains(far)].to_crs(3857).plot(ax=ax)
+    ctx.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 if __name__ == '__main__':
     app.run(debug=True)
